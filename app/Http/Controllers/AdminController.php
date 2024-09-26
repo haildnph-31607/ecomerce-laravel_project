28f4760a5f087a2ce473a4bce345263ca51d5d09
Lê Đức Ngọc Hải <nguyenminhkhong2004@gmail.com>
2024-09-26 10:03:13 +0700

    new commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function dashboard()
+    {
+        return view('admin.dashboard');
+    }
+    public function loginAdmin(){
+          return view('auth.login-admin');
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/login-admin-dashboard', [AdminController::class, 'loginAdmin']);
+
+Route::middleware('is_admin')->prefix('admin')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+});
